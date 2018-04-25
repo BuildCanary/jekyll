2edd8c947ba10548be0844f91932e29cdf532223
lkillgore <lucas.killgore@microsoft.com>
2018-04-25 17:10:53 -0400

    fix test errors

--- test/test_utils.rb
+++ test/test_utils.rb
@@ -376,16 +376,6 @@ class TestUtils < JekyllUnitTest
   end
 
   context "The \`Utils.has_yaml_header?\` method" do
-    should "accept files with YAML front matter" do
-      file = source_dir("_posts", "2008-10-18-foo-bar.markdown")
-      assert_equal "---\n", File.open(file, "rb") { |f| f.read(4) }
-      assert Utils.has_yaml_header?(file)
-    end
-    should "accept files with extraneous spaces after YAML front matter" do
-      file = source_dir("_posts", "2015-12-27-extra-spaces.markdown")
-      assert_equal "---  \n", File.open(file, "rb") { |f| f.read(6) }
-      assert Utils.has_yaml_header?(file)
-    end
     should "reject pgp files and the like which resemble front matter" do
       file = source_dir("pgp.key")
       assert_equal "-----B", File.open(file, "rb") { |f| f.read(6) }
